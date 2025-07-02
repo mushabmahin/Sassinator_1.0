3666e373b98eba420dbde31576ffc80fb6ff5f0d
Adarsh-M-Nair <adarshvaikhary@gmail.com>
2025-07-02 23:47:32 +0530

    Save local changes before pulling

--- pages/index.vue
+++ pages/index.vue
@@ -2,16 +2,18 @@
 <template>
   <Header />
   <Challenge/>
+  <Why />
   <Contact />
   <Footer/>
+  <Testimonial />
 </template>
 
 <script setup>
 import Contact from '~/components/contact.vue';
 import Header from '../components/header.vue'
-import Challenge from '../components/challenge.vue';
 
 import Footer from '../components/footer.vue'
 import Challenge from '../components/challenge.vue';
-
+import Why from '../components/why.vue';
+import Testimonial from '../components/testimonial.vue';
 </script>
