8f88bcc63564efb41290a7707bec10d6e4fdc5b7
Mushab Mahin <mushabmahin777@gmail.com>
2025-07-02 22:32:14 +0530

    Update hero and contact sections

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,10 @@
-<template>
 
+<template>
+  <Header />
+  <Contact />
 </template>
 
 <script setup>
-
+import Contact from '~/components/contact.vue';
+import Header from '../components/header.vue'
 </script>
