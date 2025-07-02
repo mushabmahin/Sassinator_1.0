d6cda7f0a2650044410fa197e64bd406779a8b1b
Mushab Mahin <mushabmahin777@gmail.com>
2025-07-02 22:41:21 +0530

    Update index and footer sections content

--- pages/index.vue
+++ pages/index.vue
@@ -2,9 +2,11 @@
 <template>
   <Header />
   <Contact />
+  <Footer/>
 </template>
 
 <script setup>
 import Contact from '~/components/contact.vue';
 import Header from '../components/header.vue'
+import Footer from '../components/footer.vue' 
 </script>
